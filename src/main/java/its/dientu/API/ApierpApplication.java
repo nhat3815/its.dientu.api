a888f09075de20cecf0b1c7462e072a3f270c3dd
nhat3815 <nhat3815@gmail.com>
2021-10-29 23:49:27 +0700

    upCode

---
+++ src/main/java/its/dientu/API/ApierpApplication.java
@@ -0,0 +1,13 @@
+package its.dientu.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApierpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApierpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/its/dientu/API/ApierpApplicationTests.java
@@ -0,0 +1,13 @@
+package its.dientu.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApierpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
